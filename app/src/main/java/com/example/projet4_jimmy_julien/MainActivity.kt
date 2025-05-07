019541973380d6dced436dab817c783b049aca73
GuibordJulien <200848756@collegeahuntsic.qc.ca>
2025-05-07 12:37:37 -0400

    Commit Test 3 Julien

--- app/src/main/java/com/example/projet4_jimmy_julien/MainActivity.kt
+++ app/src/main/java/com/example/projet4_jimmy_julien/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Projet4_JimmyJulienTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android terst",
+                        name = "Android testt 3",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
