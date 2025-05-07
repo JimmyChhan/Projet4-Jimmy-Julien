7dc337ccb9c1cabfc8c11ae708c10f2528681cff
Jimmy.Chhan <201966608@collegeahuntsic.qc.ca>
2025-05-07 12:21:11 -0400

    Test

--- app/src/main/java/com/example/projet4_jimmy_julien/MainActivity.kt
+++ app/src/main/java/com/example/projet4_jimmy_julien/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Projet4_JimmyJulienTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Test",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
