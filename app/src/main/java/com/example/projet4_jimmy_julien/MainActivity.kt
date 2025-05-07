0bc048708fce1ada30110a112ec6f89172a891e5
GuibordJulien <200848756@collegeahuntsic.qc.ca>
2025-05-07 12:36:45 -0400

    Commit Test Julien

--- app/src/main/java/com/example/projet4_jimmy_julien/MainActivity.kt
+++ app/src/main/java/com/example/projet4_jimmy_julien/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Projet4_JimmyJulienTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android terst",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
