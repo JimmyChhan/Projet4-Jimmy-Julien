9a6ae5b4f520ebf80a9b0509224cc407e982255e
GuibordJulien <200848756@collegeahuntsic.qc.ca>
2025-05-12 15:32:01 -0400

    TodoDao

---
+++ app/src/main/java/com/example/projet4_jimmy_julien/data/TodoDao.kt
@@ -0,0 +1,28 @@
+package com.example.inventory.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.projet4_jimmy_julien.data.Todo
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TodoDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(todo: Todo)
+
+    @Update
+    suspend fun update(todo: Todo)
+
+    @Delete
+    suspend fun delete(todo: Todo)
+
+    @Query("SELECT * from todos WHERE id = :id")
+    fun getTodoById(id: Int): Flow<Todo>
+
+    @Query("SELECT * from todos ORDER BY dateCreation ASC")
+    fun getAllTodos(): Flow<List<Todo>>
+}
